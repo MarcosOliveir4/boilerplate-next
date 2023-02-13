b7cb87369b3d25a233c4655e9657401497da5e24
MarcosOliveir4 <marcosassis071@gmail.com>
2023-02-13 13:40:57 -0300

    fix: removendo codeclimate

--- src/pages/_document.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import MyDocument from '.';
-
-describe('<MyDocument />', () => {
-  it('renders the document correctly', () => {
-    const { container } = render(<MyDocument />);
-    expect(container).toBeTruthy();
-  });
-});
